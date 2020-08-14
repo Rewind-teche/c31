4d38f8d69fbad7dd7959fb6ff1e698004a2e2d35
Rewind-teche <67373977+Rewind-teche@users.noreply.github.com>
2020-08-14 15:19:09 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smoke_image = loadImage("sprites/smoke.png")
+    this.trejectory = []
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+    var position = [this.body.position.x,this.body.position.y]
+    this.trejectory.push(position)
+    }
+    for(var i = 0;i<this.trejectory.length;i++)
+    {
+       image(this.smoke_image,this.trejectory[i][0],this.trejectory[i][1])
+       console.table(this.trejectory[i][0])
+    }
+    super.display();
+  
+  }
+}
